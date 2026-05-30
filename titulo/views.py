e4d70d5558aeaeb09d9689954bbd6beb062bccd0
felipemmarinho <felipemmarinho@hotmail.com>
2026-05-29 21:56:01 -0300

    aula8

--- titulo/views.py
+++ titulo/views.py
@@ -2,29 +2,8 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
-def show_view(request):
-    return HttpResponse( "<p> Mnha view do app Títulos </p>")
-
-def show_template(request):
-    return HttpResponse( "<p> Minha view do app Títulos usando template </p>")
-
-def listar_exemplo(request):
-    pagina='Olá'
-    return HttpResponse(pagina)
-
-def abc(request):
-    pagina = 'ABC'
-    return HttpResponse(pagina)
-
-def index(request):
-    return render(request, 'index.html')
-
-
-
-
-
-
-
-
-
+def listar(request):
+    return render(request, 'titulo/listarTitulos.html')
+def cadastrar(request):
+    return render(request, 'titulo/cadastroTitulos.html')
 
